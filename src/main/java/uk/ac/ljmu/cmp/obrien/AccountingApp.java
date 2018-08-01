51d6ae65654bf6366a02aa6270c25d397e94316d
Hui <>
2018-08-01 20:35:22 +0100

    first

---
+++ src/main/java/uk/ac/ljmu/cmp/obrien/AccountingApp.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.ac.ljmu.cmp.obrien;
+
+/**
+ *
+ * @author HF
+ */
+public class AccountingApp {
+    
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
